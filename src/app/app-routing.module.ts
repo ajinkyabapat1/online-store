bd5da3904f124b03a91f0b8605eec3bb07538cf3
Ajinkya Bapat <ajinkyabapat1@gmail.com>
2020-01-03 17:47:15 +0530

    installed MAterial, header, fotter , about us comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './shared/about-us/about-us.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'AboutUsComponent',pathMatch:"full"},
+  {path:'about', component:AboutUsComponent},
+  {path:'**', component:AboutUsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/header/header.component';
+import { AboutUsComponent } from './shared/about-us/about-us.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MaterialModule } from './material.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutUsComponent,
+    FooterComponent,
+  
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
